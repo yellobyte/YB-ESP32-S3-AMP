db2e918cd11a905d38ff2150d89559f99485fa46
Thomas Jentzsch <76921504+yellobyte@users.noreply.github.com>
2025-12-31 00:19:23 +0100

    Update Play-Local-Media.ino

--- examples/ArduinoIDE/Play-Local-Media/Play-Local-Media.ino
+++ examples/ArduinoIDE/Play-Local-Media/Play-Local-Media.ino
@@ -9,7 +9,7 @@
   GPIO47 (onboard LED IO47) with the amps SD_MODE pin. Setting GPIO47 to LOW (LED off) will then shut down (mute) both
   amps and setting GPIO47 to HIGH (LED on) will activate them.
 
-  Last updated 2025-04-06, ThJ <yellobyte@bluewin.ch>
+  Last updated 2025-12-31, ThJ <yellobyte@bluewin.ch>
 */
 
 #include <Arduino.h>
@@ -47,7 +47,7 @@ void setup()
     Serial.print(".");
     delay(2000);
   }
-  Serial.printf("\nconnected successfully to \"%s\". IP address: %s\n", ssid, WiFi.localIP().toString());
+  Serial.printf("\nconnected successfully to \"%s\". IP address: %s\n", ssid, WiFi.localIP().toString().c_str());
 
   audio.setPinout(I2S_BCLK, I2S_LRCLK, I2S_DOUT);
   audio.setVolume(8);                  // 0...21(max)
