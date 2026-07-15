fa4b46d7b248ef6dfd388729812c39d7b92fb04b
Thomas Jentzsch <76921504+yellobyte@users.noreply.github.com>
2026-07-15 16:24:36 +0200

    Update Play-Radio-Station.ino

--- examples/ArduinoIDE/Play-Radio-Station/Play-Radio-Station.ino
+++ examples/ArduinoIDE/Play-Radio-Station/Play-Radio-Station.ino
@@ -1,7 +1,7 @@
 /*
   Play-Radio-Station
 
-  Plays a radio station's audio stream over the 2 MAX98357A I2S amplifiers (left+right audio channels).
+  Plays a radio station's audio stream over the two MAX98357A I2S amplifiers (left+right audio channels).
   Make sure you have library "ESP32-audioI2S" added to your project or ArduinoIDE. More info about this versatile lib
   are available on its homepage "https://github.com/schreibfaul1/ESP32-audioI2S".
 
@@ -9,7 +9,7 @@
   GPIO47 (onboard LED IO47) with the amps SD_MODE pin. Setting GPIO47 to LOW (LED off) will shut down (mute) the amps
   and setting GPIO47 to HIGH (LED on) will activate the amps.
 
-  Last updated 2025-04-06, ThJ <yellobyte@bluewin.ch>
+  Last updated 2026-07-15, ThJ <yellobyte@bluewin.ch>
 */
 
 #include <Arduino.h>
@@ -27,13 +27,8 @@ const char pass[] = "MyPassword";
 
 Audio audio;
  
-void audio_info(const char *info) {     // file info
-  Serial.print("info: "); 
-  Serial.println(info);
-}
-void audio_id3data(const char *info) {  // id3 metadata
-  Serial.print("id3: ");
-  Serial.println(info);
+void my_audio_info(Audio::msg_t m) {
+    Serial.printf("%s: %s\n", m.s, m.msg);
 }
 
 void setup()
@@ -55,6 +50,7 @@ void setup()
   Serial.printf("\nconnected successfully to \"%s\". IP address: %s\n", ssid, WiFi.localIP().toString().c_str());
   digitalWrite(LED_BUILTIN, HIGH);      // status LED On
   
+  audio.audio_info_callback = my_audio_info;
   audio.setPinout(I2S_BCLK, I2S_LRCLK, I2S_DOUT);
   audio.setVolume(10);                 // 0...21(max)
   audio.setConnectionTimeout(1200,0);  // needed for some stations esp. from around the globe
