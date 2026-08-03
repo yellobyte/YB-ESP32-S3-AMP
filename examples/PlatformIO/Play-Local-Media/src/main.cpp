89a6f73aadd4e20faa8346ebaf489286222bac92
Thomas Jentzsch <76921504+yellobyte@users.noreply.github.com>
2026-08-03 17:44:57 +0200

    example updated

--- examples/PlatformIO/Play-Local-Media/src/main.cpp
+++ examples/PlatformIO/Play-Local-Media/src/main.cpp
@@ -9,29 +9,24 @@
   GPIO47 (onboard LED IO47) with the amps SD_MODE pin. Setting GPIO47 to LOW (LED off) will then shut down (mute) both
   amps and setting GPIO47 to HIGH (LED on) will activate them.
 
-  Last updated 2025-12-31, ThJ <yellobyte@bluewin.ch>
+  Last updated 2026-08-03, ThJ <yellobyte@bluewin.ch>
 */
 
 #include <Arduino.h>
 #include <WiFi.h>
 #include "Audio.h"
 
-// media files hosted on a local DLNA server, e.g.: 
+// media files hosted on a local DLNA server, e.g.:
 #define MEDIA_FILE "http://192.168.1.11:9000/disk/DLNA-PNMP3-OP01-FLAGS01700000/O0$1$8I1867788.mp3"
 //#define MEDIA_FILE "http://192.168.1.40:9790/*/H*c3*b6rb*c3*bccher/A01/A*20C*20M/1-01*20Titel*2001.mp3"
 
 const char ssid[] = "MySSID";
-const char pass[] = "MyPassword"; 
+const char pass[] = "MyPassword";
 
 Audio audio;
- 
-void audio_info(const char *info) {     // file info
-  Serial.print("info: "); 
-  Serial.println(info);
-}
-void audio_id3data(const char *info) {  // id3 metadata
-  Serial.print("id3: ");
-  Serial.println(info);
+
+void my_audio_info(Audio::msg_t m) {
+    Serial.printf("%s: %s\n", m.s, m.msg);
 }
 
 void setup()
@@ -40,8 +35,8 @@ void setup()
 #if ARDUINO_USB_CDC_ON_BOOT == 1
   // we continue only when serial port becomes available (board revision 3.x), uncomment if needed
   //while (!Serial);
-  //delay(3000);   	
-#endif	  
+  //delay(3000);
+#endif
   Serial.println();
   Serial.println("running example \"Play-Local-Media\":");
 
@@ -54,6 +49,7 @@ void setup()
   }
   Serial.printf("\nconnected successfully to \"%s\". IP address: %s\n", ssid, WiFi.localIP().toString().c_str());
 
+  audio.audio_info_callback = my_audio_info;
   audio.setPinout(I2S_BCLK, I2S_LRCLK, I2S_DOUT);
   audio.setVolume(8);                  // 0...21(max)
   audio.setConnectionTimeout(500,0);   // needed for some servers
